9da0019736a3ae69bb1f1ff42c0c50063ae956ef
Isaac Winner <110716580+isaacwinner62@users.noreply.github.com>
2023-09-26 02:22:23 -0700

    Add files via upload
    I just added 8 files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init();
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsiv";
+  } else {
+    x.className = "topnav";
+  }
+}
