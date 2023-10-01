1ad6e8c53488a787fd2e5337a999b93fe1600b6b
isaacwinner62 <winnerisaac62@gmail.com>
2023-10-01 16:16:55 -0700

    updates
    I added the routing and the backend files.

--- script.js
+++
@@ -1,9 +0,0 @@
-AOS.init();
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsiv";
-  } else {
-    x.className = "topnav";
-  }
-}
