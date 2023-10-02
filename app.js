2dbded63257c8d40aa14ec77a660d0418eb12a5e
isaacwinner62 <winnerisaac62@gmail.com>
2023-10-02 13:37:18 -0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express");
+// const express = require("express");
 
-// express app
-const app = express();
+// // express app
+// const app = express();
 
-// listen for request
-app.listen(3000);
+// // listen for request
+// app.listen(3000);
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/crypto-inves.html", { root: __dirname});
-});
+// app.get("/", (req, res) => {
+//   res.sendFile("./views/crypto-inves.html", { root: __dirname});
+// });
 
-app.get("/sign-up", (req, res) => {
-  res.sendFile("./views/sign-up.html", { root: __dirname});
-});
+// app.get("/sign-up", (req, res) => {
+//   res.sendFile("./views/sign-up.html", { root: __dirname});
+// });
